eb775b07e9d7ec10927494964cdd0c512be0f18a
J-Massoda <emilienmassoda@gmail.com>
2023-07-15 12:02:37 +0100

    ...

--- js/main.js
+++ js/main.js
@@ -51,3 +51,43 @@ var loader = document.getElementById('pre');
 window.addEventListener("load", function () {
   loader.style.display = "none"
 })
+
+// cart 
+
+// Get the add to cart button
+var addToCartButton = document.querySelector('.add-to-cart');
+
+// Add an event listener to the button
+addToCartButton.addEventListener('click', function () {
+  // Get the product name, description, and price
+  var productName = document.querySelector('h2').textContent;
+  var productDescription = document.querySelector('p:nth-of-type(2)').textContent;
+  var productPrice = document.querySelector('p:nth-of-type(3)').textContent;
+
+  // Create a new cart item object with the product information
+  var cartItem = {
+    name: productName,
+    description: productDescription,
+    price: productPrice
+  };
+
+  // Get the existing cart items from local storage
+  var cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+
+  // Add the new cart item to the existing cart items
+  cartItems.push(cartItem);
+
+  // Save the updated cart items to local storage
+  localStorage.setItem('cartItems', JSON.stringify(cartItems));
+});
+
+// search  bar
+
+var searchTimeout = null;
+
+function searchProducts() {
+  clearTimeout(searchTimeout);
+  searchTimeout = setTimeout(function () {
+    // Search function code goes here
+  }, 500); // Delay in milliseconds
+}
